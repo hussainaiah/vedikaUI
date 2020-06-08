e74f4f26c9d2565e7dbb8fe4e37ab11043167fea
hussainaiah <hussainaiahkarumanchi@gmail.com>
2020-06-08 12:23:48 +0530

    this is updated commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FunctionhallComponent } from './functionhall/functionhall.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HelpComponent } from './help/help.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SearchbarService } from './searchbar/searchbar.service';
+import { FunctionhallpageComponent } from './functionhall/functionhallpage/functionhallpage.component';
+import { RegisterComponent } from './register/register.component';
+ 
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FunctionhallComponent,
+    SearchbarComponent,
+    HelpComponent,
+    LoginComponent,
+    FunctionhallpageComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule, ReactiveFormsModule,
+    AppRoutingModule,
+    FormsModule,
+    Ng2SearchPipeModule,
+    HttpClientModule,
+  ],
+  providers: [SearchbarService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
